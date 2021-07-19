d93ba421f3d41f6241f2b7879fed0fedd5e4d383
Edomak <edoardo.maccherini@gmail.com>
2021-07-19 13:51:44 +0200

    migration e seeder

---
+++ database/migrations/2021_07_19_112716_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb', 255);
+            $table->decimal('price', 5, 2);
+            $table->string('series', 100);
+            $table->string('sale_date', 10);
+            $table->string('type', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
